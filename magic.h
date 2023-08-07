c2902c98bcb45538ef4e9f7a50364e5ba7c6cbc3
kiwataSasakiLab <kazuhisa.iwata.7d@stu.hosei.ac.jp>
2023-08-07 21:06:53 +0900

    add

---
+++ magic.h
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <new>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <complex>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+
+using namespace std;
